22f13baa8867f45760e3558363ce0dfe14accc73
Nikita <nikita6370@gmail.com>
2020-06-14 19:09:11 +0300

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+// const user = 'Ivan';
+// alert(`приве, ${user}`);
+
+
+// const isChecked = true,
+// isClose = false;
+// console.log(isChecked || isClose);
+if indexedDB.html.getElementsByClassName("promo_btn") is onclick{
+    const answer1 = prompt('вы действительно этого хотиите?');
+    if (answer1 == 'да'){
+    alert ("не стоит")
+    } else {
+    alert ("иди нахуй лох")
+    }
+}
+
+
